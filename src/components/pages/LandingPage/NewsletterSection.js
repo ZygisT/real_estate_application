8c0927247e0603181e18e298a4c30fc5aa0e9db3
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-24 18:12:08 +0300

    Newsletter section development

--- src/components/pages/LandingPage/NewsletterSection.js
+++ src/components/pages/LandingPage/NewsletterSection.js
@@ -4,7 +4,24 @@ export default class NewsletterSection extends Component {
   render() {
     return (
       <section id="newsletter-section">
-        Newsletter
+        
+        <div className="center-content">
+
+          <div className="newsletter-section-items-container">
+
+            <div className="subscribe-message">
+            <h4>Sign Up for greatest deals on real estate!</h4>
+            </div>
+
+            <div className="subscribe-form">
+              <input className='subscribe-input' type="text" name='email' placeholder='Enter your email'/>
+
+              <a href="#newsletter-section" className="subscribe-btn">Subscribe</a>
+            </div>
+          </div>
+
+        </div>
+
       </section>
     )
   }
