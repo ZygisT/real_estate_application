8c2606c0638d17ed5d41b6a80d345e4d422c48fb
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-25 23:43:40 +0300

    Search form development

--- src/components/pages/ListingsPage.js
+++ src/components/pages/ListingsPage.js
@@ -14,20 +14,45 @@ export default class ListingsPage extends Component {
               <h3>Search for properties:</h3>
 
               <div className="fields-wrapper">
-                <label htmlFor="">Property status</label>
-                <select name="properyStatus" id="propertyStatus">
-                  <option value="any">Any</option>
-                </select>
 
-                <label htmlFor="">Property type</label>
-                <select name="properyType" id="propertyType">
-                  <option value="any">Any</option>
-                </select>
+                <div className="top-block">
+                  <label htmlFor="properyStatus">Property status</label>
+                  <select name="properyStatus" id="propertyStatus">
+                    <option value="any">Any</option>
+                  </select>
 
-                <label htmlFor="">Location</label>
-                <select name="properyLocation" id="propertyLocation">
-                  <option value="any">Any</option>
-                </select>
+                  <label htmlFor="propertyType">Property type</label>
+                  <select name="properyType" id="propertyType">
+                    <option value="any">Any</option>
+                  </select>
+
+                  <label htmlFor="propertyLocation">Location</label>
+                  <select name="properyLocation" id="propertyLocation">
+                    <option value="any">Any</option>
+                  </select>
+                </div>
+
+                <div className="bottom-block">
+                  
+                  <label htmlFor="Price">Price</label>
+                  <div className="input-container">
+                    <input type="number" value='min' placeholder='Min'/>
+                    <input type="number" value='max' placeholder='Max'/>
+                  </div>
+                  
+                  <label htmlFor="Bedrooms">Bedrooms</label>
+                  <div className="input-container">
+                    <input type="number" value='min' placeholder='Min'/>
+                    <input type="number" value='max' placeholder='Max'/>
+                  </div>
+                  
+                  <label htmlFor="Bathrooms">Bathrooms</label>
+                  <div className="input-container">
+                    <input type="number" value='min' placeholder='Min'/>
+                    <input type="number" value='max' placeholder='Max'/>
+                  </div>
+
+                </div>
 
               </div>
 
