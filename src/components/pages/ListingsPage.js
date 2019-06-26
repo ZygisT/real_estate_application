063a42da639374f41bd63e5ba16c301f789bff5c
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-26 23:02:10 +0300

    Sort by area completed

--- src/components/pages/ListingsPage.js
+++ src/components/pages/ListingsPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Filter from './ListingsPage/Filter'
+import Listings from './ListingsPage/Listings'
 
 export default class ListingsPage extends Component {
   render() {
@@ -12,27 +13,7 @@ export default class ListingsPage extends Component {
           <div className="content-wrapper">
 
             <div className="listings-area">
-              
-              <section id="sort-by-area">
-
-                <div className="search-resulets">12 results found</div>
-
-                <span className="sort-by">Sort by</span>
-
-                <div className="sort-options">
-                  <select name="sort-by-price" id="sort-by-price">
-                    <option value="price-dsc">Price Lowest</option>
-                    <option value="price-asc">Price Highest</option>
-                  </select>
-                  <div className="view-mode">
-                    <i class="fas fa-th view-mode-icon"></i>
-                    <i class="fas fa-th-list view-mode-icon"></i>
-                  </div>
-                </div>
-              
-
-              </section>
-
+              <Listings />
             </div>
 
             <div className="filter-area">
--- src/components/pages/ListingsPage/Listings.js
+++ src/components/pages/ListingsPage/Listings.js
@@ -1,16 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Listings extends Component {
-  constructor () {
-    super()
-    this.state = {
-    }
+  constructor() {
+    super();
+    this.state = {};
   }
   render() {
     return (
-      <div>
-        
+      // Sort by area start
+      <div id="sort-by-area">
+
+        <div className="results-container">
+          <div className="search-results">12 results found</div>
+        </div>
+
+        <div className="sort-container">
+
+
+          <div className="sort-options">
+
+            <span className="sort-by-text">Sort by:</span>
+            <select className='sort-by-price-select' name="sort-by-price" id="sort-by-price">
+              <option value="all">All</option>
+              <option value="price-dsc">Price (Low to High)</option>
+              <option value="price-asc">Price (High to Low)</option>
+            </select>
+            
+          </div>
+
+          <div className="view-mode">
+
+              <i className="fas fa-th view-mode-icon is-active" />
+              <i className="fas fa-th-list view-mode-icon" />
+          
+          </div>
+
+        </div>
+
+
       </div>
-    )
+      // Sort by area end
+    );
   }
 }
