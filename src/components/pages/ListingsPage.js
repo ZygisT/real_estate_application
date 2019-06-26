383050154944fe117f23f55c06d22b3ed4286639
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-26 21:44:27 +0300

    listings sortby area feature development

--- src/components/pages/ListingsPage.js
+++ src/components/pages/ListingsPage.js
@@ -11,13 +11,33 @@ export default class ListingsPage extends Component {
           
           <div className="content-wrapper">
 
-           <div className="filter-area">
-             <Filter />
-           </div>
+            <div className="listings-area">
+              
+              <section id="sort-by-area">
 
-           <div className="listings-area">
-             
-           </div>
+                <div className="search-resulets">12 results found</div>
+
+                <span className="sort-by">Sort by</span>
+
+                <div className="sort-options">
+                  <select name="sort-by-price" id="sort-by-price">
+                    <option value="price-dsc">Price Lowest</option>
+                    <option value="price-asc">Price Highest</option>
+                  </select>
+                  <div className="view-mode">
+                    <i class="fas fa-th view-mode-icon"></i>
+                    <i class="fas fa-th-list view-mode-icon"></i>
+                  </div>
+                </div>
+              
+
+              </section>
+
+            </div>
+
+            <div className="filter-area">
+              <Filter />
+            </div>
 
           </div>
 
