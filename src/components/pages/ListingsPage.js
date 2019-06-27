2d7d23c0322f41983348fb7fb7c5bae694fda0ee
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-27 20:41:12 +0300

    Listings mapping and filtering development

--- src/components/pages/ListingsPage.js
+++ src/components/pages/ListingsPage.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 import Filter from './ListingsPage/Filter'
 import Listings from './ListingsPage/Listings'
+import listingsData from '../../data/listingsData'
 
 export default class ListingsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      listingsData,
+
+    };
+  }
+
   render() {
     return (
       // Listings Section Start
@@ -13,11 +22,11 @@ export default class ListingsPage extends Component {
           <div className="content-wrapper">
 
             <div className="listings-area">
-              <Listings />
+              <Listings listingsData={this.state.listingsData}/>
             </div>
 
             <div className="filter-area">
-              <Filter />
+              {/* <Filter /> */}
             </div>
 
           </div>
--- src/components/pages/ListingsPage/Listings.js
+++ src/components/pages/ListingsPage/Listings.js
@@ -6,91 +6,91 @@ export default class Listings extends Component {
     this.state = {};
   }
 
-  
+  loopListings = () => {
+    const { listingsData } = this.props;
+
+    return listingsData.map((listing, i) => {
+
+      return (
+        <div key={i} className="listing-card">
+          <div className="listing-img" style={{background: `url(${listing.image}) no-repeat center`, backgroundSize: 'cover'}}>
+            <span className="listing-status">{listing.status}</span>
+          </div>
+
+          <div className="listing-features-container">
+            <span className="feature">
+              <i className="far fa-square feature-icon" />{listing.floorSpace} m&sup2;
+            </span>
+
+            <span className="feature">
+              <i className="fas fa-bed feature-icon" />{listing.bedrooms}
+            </span>
+
+            <span className="feature">
+              <i className="fas fa-bath feature-icon" />{listing.bathrooms}
+            </span>
+          </div>
+
+          <div className="price-and-description-container">
+            <div className="listing-price">${listing.price.toLocaleString()}</div>
+
+            <div className="listing-description">
+              <p>
+                {listing.description}
+              </p>
+            </div>
+          </div>
+
+          <div className="location-and-btn-container">
+            <span className="listing-location">
+              <i className="fas fa-map-marker-alt address-marker" />{listing.address}, {listing.city}, {listing.state}
+            </span>
+
+            <a className="primary-btn" href="#sort-by-area">
+              Read More
+            </a>
+          </div>
+        </div>
+      );
+    });
+  };
 
   render() {
     return (
       <section className="listings-wrapper">
-
         <section id="sort-by-area">
-  
           <div className="results-container">
             <div className="search-results">12 results found</div>
           </div>
-  
+
           <div className="sort-container">
-  
-  
             <div className="sort-options">
-  
               <span className="sort-by-text">Sort by:</span>
-              <select className='sort-by-price-select' name="sort-by-price" id="sort-by-price">
+              <select
+                className="sort-by-price-select"
+                name="sort-by-price"
+                id="sort-by-price"
+              >
                 <option value="all">All</option>
                 <option value="price-dsc">Price (Low to High)</option>
                 <option value="price-asc">Price (High to Low)</option>
               </select>
-              
             </div>
-  
+
             <div className="view-mode">
-  
-                <i className="fas fa-th view-mode-icon is-active" />
-                <i className="fas fa-th-list view-mode-icon" />
-            
+              <i className="fas fa-th view-mode-icon is-active" />
+              <i className="fas fa-th-list view-mode-icon" />
             </div>
-  
           </div>
-  
-  
         </section>
 
         <section id="listings-container">
 
-          <div className="listing-card">
-
-            <div className="listing-img">
-              <span className="listing-status">Sale</span>
-            </div>
-
-            <div className="listing-features-container">
-
-              <span className="feature">
-                <i className="far fa-square feature-icon"></i>210 m&sup2;
-              </span>
-
-              <span className="feature">
-                <i className="fas fa-bed feature-icon"></i>3
-              </span>
-
-              <span className="feature">
-                <i className="fas fa-bath feature-icon"></i>4
-              </span>
-
-            </div>
-
-            <div className="price-and-description-container">
-              <div className="listing-price">$180,000</div>
-              
-              <div className="listing-description">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vitae dolor labore placeat libero dolorum rerum eaque praesentium expedita in.</p>
-              </div>
-            </div>
-
-            <div className="location-and-btn-container">
-              <span className="listing-location">
-                <i className="fas fa-map-marker-alt address-marker"></i>Address
-              </span>
-
-              <a className='primary-btn' href="#sort-by-area">Read More</a>
-            </div>
-          
-          </div>
-          
+          {this.loopListings()}
 
         </section>
 
       </section>
-
     );
   }
 }
