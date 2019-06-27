343f3af21bf424da02f87aebc05d6d233e11ae42
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-06-27 16:49:36 +0300

    Updating database by adding description for each item in the database

--- src/components/pages/ListingsPage/Listings.js
+++ src/components/pages/ListingsPage/Listings.js
@@ -7,39 +7,48 @@ export default class Listings extends Component {
   }
   render() {
     return (
-      // Sort by area start
-      <div id="sort-by-area">
+      <section className="listings-wrapper">
 
-        <div className="results-container">
-          <div className="search-results">12 results found</div>
-        </div>
-
-        <div className="sort-container">
-
-
-          <div className="sort-options">
-
-            <span className="sort-by-text">Sort by:</span>
-            <select className='sort-by-price-select' name="sort-by-price" id="sort-by-price">
-              <option value="all">All</option>
-              <option value="price-dsc">Price (Low to High)</option>
-              <option value="price-asc">Price (High to Low)</option>
-            </select>
+        <section id="sort-by-area">
+  
+          <div className="results-container">
+            <div className="search-results">12 results found</div>
+          </div>
+  
+          <div className="sort-container">
+  
+  
+            <div className="sort-options">
+  
+              <span className="sort-by-text">Sort by:</span>
+              <select className='sort-by-price-select' name="sort-by-price" id="sort-by-price">
+                <option value="all">All</option>
+                <option value="price-dsc">Price (Low to High)</option>
+                <option value="price-asc">Price (High to Low)</option>
+              </select>
+              
+            </div>
+  
+            <div className="view-mode">
+  
+                <i className="fas fa-th view-mode-icon is-active" />
+                <i className="fas fa-th-list view-mode-icon" />
             
+            </div>
+  
           </div>
+  
+  
+        </section>
 
-          <div className="view-mode">
+        <div className="listings-container">
 
-              <i className="fas fa-th view-mode-icon is-active" />
-              <i className="fas fa-th-list view-mode-icon" />
           
-          </div>
 
         </div>
 
+      </section>
 
-      </div>
-      // Sort by area end
     );
   }
 }
