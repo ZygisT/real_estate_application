5630a0c63a411b88e95a7ba1819493c054322d8f
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-07-01 22:01:18 +0300

    Post card development

--- src/components/pages/BlogPage.js
+++ src/components/pages/BlogPage.js
@@ -29,6 +29,39 @@ export default class BlogPage extends Component {
             </div>
           </section>
 
+          <section id="post-section">
+
+            <div className="post-card">
+
+              <h3 className="topic-title">This is the title of the topic.</h3>
+
+              <div className="post-details">
+                <span className="username">Emillia Warren</span>
+                <span className="time-posted"> - June 7, 2019</span>
+              </div>
+            
+              <div className="post-img"></div>
+
+              <div className="post-topic">
+                <p>Think you have a “photogenic side” for camera? So does your home! We will teach you how to take interesting picture of your house to make your listings stand out!</p>
+              </div>
+
+              <div className="misc">
+                <div className="comment-count">
+                  <i className="far fa-comment-alt comment-icon"></i> 0 comments
+                </div>
+                <div className="tags-box">
+                  <span>Tags:</span>
+                  <div className="tags-container">
+                    <span className="tag">Apartment</span>
+                    <span className="tag">New</span>
+                    <span className="tag">Modern</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </section>
         </div>
 
       </section>
