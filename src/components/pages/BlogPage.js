4e672b77b8fb36413ef8b3bbea451ad47e92b710
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-07-01 19:36:45 +0300

    Search bar feature development

--- src/components/pages/BlogPage.js
+++ src/components/pages/BlogPage.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react'
 export default class BlogPage extends Component {
   render() {
     return (
-      <div>
-        This is the blog page.
-      </div>
+      <section id="blog-page">
+
+        <div className="center-content">
+
+          <section id="search-section">
+            <h3>Search</h3>
+            <input className="search-input" type="text" placeholder="Enter keyword"/>
+          </section>
+
+        </div>
+
+      </section>
     )
   }
 }
