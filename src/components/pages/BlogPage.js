82a02ad6a69e021e0328041d793dc640dbb6abcc
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-07-01 20:49:44 +0300

    Search and sort features completed, blog post card development

--- src/components/pages/BlogPage.js
+++ src/components/pages/BlogPage.js
@@ -9,7 +9,24 @@ export default class BlogPage extends Component {
 
           <section id="search-section">
             <h3>Search</h3>
-            <input className="search-input" type="text" placeholder="Enter keyword"/>
+            <input className="search-bar" type="text" placeholder="Enter keyword..."/>
+          </section>
+
+          <section id="sort-section">
+            <div className="sort-container">
+              <span className="sort-title">Sort by:</span>
+              <div className="sort-options">
+                <select
+                  className="sort-by-time-select"
+                  name="sortby"
+                >
+                  <option value="all">All</option>
+                  <option value="time-newest">Posts (New to Old)</option>
+                  <option value="time-oldest">Posts (Old to New)</option>
+                </select>
+              </div>
+
+            </div>
           </section>
 
         </div>
