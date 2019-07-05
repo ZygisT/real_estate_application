c8d3b7fbae8fcf65cf5eee1ddda527f337f0d37a
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-07-05 22:42:13 +0300

    Created static database for agents data, looped thru data to generate agents listing cards, agents page has been completed

--- src/components/pages/AgentsPage.js
+++ src/components/pages/AgentsPage.js
@@ -1,38 +1,59 @@
 import React, { Component } from 'react'
+import agentsData from '../../data/agentsData'
 
 export default class AgentsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      agentsData
+    }
+  }
+
+  loopAgentsData = () => {
+    return this.state.agentsData.map((item, i) => {
+      return (
+        <div key={i} className="agent-card">
+
+        <div className="card-img" style={{backgroundImage: `url(${item.agentImage})`}}></div>
+
+        <div className="agent-details">
+
+          <h4 className="agent-name">{item.agentName}</h4>
+
+          <div className="agent-description">
+            <p>{item.agentDescription}</p>
+          </div>
+
+          <span className="contact-number"><i className="fas fa-phone-alt fa-fw icon"></i> {item.agentContactNumber}</span>
+
+          <div className="social-media-container">
+            <a className='media-link' href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f fa-fw media-icon"></i></a>
+            <a className='media-link' href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter fa-fw media-icon"></i></a>
+            <a className='media-link' href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-fw media-icon"></i></a>
+          </div>
+
+          <a href="#agents-page" className="primary-btn">Read More</a>
+        </div>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <section id="agents-page">
         <div className="center-content">
-          <div className="agents-listing-items-container">
+          <div className="agents-listing-container">
             <div className="section-header">      
               <h3>Agents Listing</h3>
             </div>
 
-            <div className="agent-card">
+            <div className="agents-card-container">
 
-              <div className="card-img"></div>
-
-              <div className="agent-details">
-
-                <h4 className="agent-name">Agent Name</h4>
-
-                <div className="agent-description">
-                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic reiciendis inventore rem temporibus expedita voluptates natus illum dolorum porro similique?</p>
-                </div>
-
-                <span className="contact-number"><i className="fas fa-phone-alt fa-fw icon"></i> 800-1234-5678</span>
-
-                <div className="social-media-container">
-                  <a className='media-link' href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f fa-fw media-icon"></i></a>
-                  <a className='media-link' href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter fa-fw media-icon"></i></a>
-                  <a className='media-link' href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-fw media-icon"></i></a>
-                </div>
-
-                <a href="#agents-page" className="primary-btn">Read More</a>
-              </div>
+              {this.loopAgentsData()}
             </div>
+
+            
           </div>
         </div>
       </section>
---
+++ src/data/agentsData.js
@@ -0,0 +1,28 @@
+const agentsData = [
+  {
+    agentImage: 'https://www.pedersenandpartners.com/sites/default/files/public/goran-civkarovski-s.jpg',
+    agentName: 'Thomas O\'Connor',
+    agentDescription: 'Hello. I am Thomas O\'Connor a real estate agent. I have been real estate agent for 22 years and have been working in Better Home real estate company the entire time. I specialize in sales and will help you to choose your new home.',
+    agentContactNumber: '800-1234-5678',
+  },
+  {
+    agentImage: 'https://www.wallisphoto.com/IMAGES/2019/corporate/Woman-business-headshot-thm.jpg',
+    agentName: 'Victoria Rodrigues',
+    agentDescription: 'I\'m Victoria Rodrigues. My expertise are sales and rents. I mainly focus on California area. I will gladly help you on phone or in person consulating as well as house touring and searching.',
+    agentContactNumber: '800-8765-4321',
+  },
+  {
+    agentImage: 'https://live.staticflickr.com/5311/14365669046_06540bbbff_b.jpg',
+    agentName: 'Isabella Smith',
+    agentDescription: 'My name is Isabella Smith. I am real estate agent working at Better Home. During my 15 years of being real estate agent I have accumulated knowledage and experience in real estate business world.',
+    agentContactNumber: '800-1122-3344',
+  },
+  {
+    agentImage: 'https://skytechuav.com.au/wp-content/uploads/2017/08/Buisnessman.png',
+    agentName: 'Oscar White',
+    agentDescription: 'I am Oscar White. Real estate agent at Better Home. I have been working as real estate agent for nearly 20 years. I mainly specialize in sales and rents of luxurious houses in Florida and California.',
+    agentContactNumber: '800-3344-5555',
+  },
+]
+
+export default agentsData;
