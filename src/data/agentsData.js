8207505c220bf454d7cb528844f4caa514a33fe2
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-07-31 13:00:51 +0300

    Optimized on some of the application pages, restructured folders, added new paths to images

--- src/data/agentsData.js
+++ src/data/agentsData.js
@@ -1,24 +1,24 @@
 const agentsData = [
   {
-    agentImage: 'https://www.pedersenandpartners.com/sites/default/files/public/goran-civkarovski-s.jpg',
+    agentImage: './agents-images/agent_1.jpg',
     agentName: 'Thomas O\'Connor',
     agentDescription: 'Hello. I am Thomas O\'Connor a real estate agent. I have been real estate agent for 22 years and have been working in Find Home real estate company the entire time. I specialize in sales and will help you to choose your new home.',
     agentContactNumber: '800-1234-5678',
   },
   {
-    agentImage: 'https://www.wallisphoto.com/IMAGES/2019/corporate/Woman-business-headshot-thm.jpg',
+    agentImage: './agents-images/agent_2.jpg',
     agentName: 'Victoria Rodrigues',
     agentDescription: 'I\'m Victoria Rodrigues. My expertise are sales and rents. I mainly focus on California area. I will gladly help you on phone or in person consulating as well as house touring and searching.',
     agentContactNumber: '800-8765-4321',
   },
   {
-    agentImage: 'https://www.littlesphotography.com/wp-content/uploads/2018/09/Moser-3472.jpg',
+    agentImage: './agents-images/agent_3.jpg',
     agentName: 'Isabella Smith',
     agentDescription: 'My name is Isabella Smith. I am real estate agent working at Find Home. I am specializing in sales and I have accumulated knowledage and experience in real estate business world which I use to help our clients to find their dream homes.',
     agentContactNumber: '800-1122-3344',
   },
   {
-    agentImage: 'https://skytechuav.com.au/wp-content/uploads/2017/08/Buisnessman.png',
+    agentImage: './agents-images/agent_4.jpg',
     agentName: 'Oscar White',
     agentDescription: 'I am Oscar White. Real estate agent at Find Home. I have been working as real estate agent for nearly 20 years. I mainly specialize in sales and rents of luxurious houses in Florida and California.',
     agentContactNumber: '800-3344-5555',
