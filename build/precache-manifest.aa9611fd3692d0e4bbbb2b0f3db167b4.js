228a29d4a471f8029f089fc5ced05c2ab02dd92e
Zygimantas Trepkevicius <matt.ishtar@yahoo.com>
2019-07-31 21:31:52 +0300

    Created build folder

---
+++ build/precache-manifest.aa9611fd3692d0e4bbbb2b0f3db167b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95916fc03264460387471ed60a8b1b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a97f356eef78d73f7004",
+    "url": "/static/css/main.8317525b.chunk.css"
+  },
+  {
+    "revision": "3de0017d7e5108fb4fcf",
+    "url": "/static/js/2.04da4d56.chunk.js"
+  },
+  {
+    "revision": "a97f356eef78d73f7004",
+    "url": "/static/js/main.b2c57141.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "58b255e6305b5a07aa5da90d19fc0863",
+    "url": "/static/media/about-section-image.58b255e6.webp"
+  },
+  {
+    "revision": "4047af8140b6b880f8747bc13ac87102",
+    "url": "/static/media/landing-image.4047af81.webp"
+  }
+]);
